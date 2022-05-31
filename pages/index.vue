82b08b01e5bdfc5728839f8e1b2592b95ede6eb6
KeithLaww <90820000+KeithLaww@users.noreply.github.com>
2022-05-31 23:11:36 +0800

    Vue Route to Login + Changes on index
    updated code on index
    routed login from landing page to login page
    
    Co-Authored-By: vanleexn <93701568+vanleexn@users.noreply.github.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,20 @@
 <template>
   <html>
-    <h1 style="color:#BC4749;font-family:merriweather"><strong><i>Here's<br>Our<br>Plan<br><br></i></strong></h1>
-    <body style="background-color:#F2E8CF;">
+    <h1 style="color:#BC4749; font-family:merriweather; font-size: 70px; padding-left: 50px;">
+      <strong>
+      <i>
+        Here's<br>
+        Our<br>
+        Plan<br>
+      </i>
+      </strong>
+    </h1>
+    
+    <body style="background-color:#FCF6E7;">
     <h4 align='center'><strong>Welcome!</strong></h4>
     <form action="" method="post">
     <style> 
-      input[type=button], input[type=submit], input[type=reset] {
+      input[type=button]{
       background-color: #A7C957;
       border: none;
       color: white;
@@ -16,19 +25,38 @@
       align: center;
       border-radius: 15px;
       font-style: italic;
-      text-align: center; 
     }
     </style>
     <div class="buttonHolder">
-      <input type='submit' value = "Log In">
+      <router-link to ="/loginpage">
+      <input type='button' style="color: #386641; font-weight: bold;" value = "Log In">
+    </router-link>
     </div>
     <br>
   
     <h6 align='center'>New User?</h6>
     <div class="buttonHolder">
-      <input type='submit' value = "Register">
+      <input type='submit' style="color: #BC4749; font-weight: bold;" value = "Register">
     </div>
-    <style>.buttonHolder{ text-align: center; }</style>
+    <style> 
+      input[type=submit]{
+      background-color: #F2E8CF;
+      border: none;
+      color: white;
+      padding: 5px 100px;
+      text-decoration: none;
+      margin: 2px 1px;
+      cursor: pointer;
+      align: center;
+      border-radius: 15px;
+      font-style: italic;
+    }
+    </style>
+
+    <style>
+    .buttonHolder{ text-align: center; }
+    </style>
+
     </form>
     </body>
   </html>
@@ -36,7 +64,9 @@
 </template>
 
 <script>
+
 export default {
   name: 'IndexPage',
 }
+
 </script>
---
+++ pages/loginpage.vue
@@ -0,0 +1,62 @@
+<template>
+  <html>
+    <h1 style="color:#BC4749; font-family:merriweather; font-size: 70px; padding-left: 50px;">
+      <strong>
+      <i>
+        Here's<br>
+        Our<br>
+        Plan<br>
+      </i>
+      </strong>
+    </h1>
+    
+    <body style="background-color:#FCF6E7;">
+    <form action="" method="post">
+    <style> 
+      input[type=button] {
+      background-color: #A7C957;
+      border: none;
+      color: white;
+      padding: 5px 100px;
+      text-decoration: none;
+      margin: 2px 1px;
+      cursor: pointer;
+      align: center;
+      border-radius: 15px;
+      font-style: italic;
+      text-align: center; 
+    }
+    </style>
+    <div class="loginDetails">
+        <input type="text" placeholder="Username" style = "background-color: #F2E8CF; width: 17%;
+        color: black; border: 5px solid #F2E8CF; border-radius: 10px; box-sizing: border-box;">
+        <br/><br/>
+        <input type="text" placeholder="Password" style = "background-color: #F2E8CF; width: 17%;
+        color: black; border: 5px solid #F2E8CF; border-radius: 10px; box-sizing: border-box;">
+        <br/><br/>
+    </div>
+
+    <style>
+    .loginDetails{ text-align: center; }
+    </style>
+
+    <div class="buttonHolder">
+      <input type='button' style="color: #386641; font-weight: bold;" value = "Log In">
+    </div>
+    <br>
+    <style>.buttonHolder{ text-align: center; }</style>
+
+    <router-link to ="">
+        <p align='center' style="color: black;"><i><u>Forgot your password?</u></i></p>
+    </router-link>
+    </form>
+    </body>
+  </html>
+  
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
