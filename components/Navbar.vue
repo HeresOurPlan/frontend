152b1472e049d1604d083f37e3a5ac331045bf8f
vanleexn <vanessa.lee.2021@scis.smu.edu.sg>
2022-06-15 03:10:44 +0800

    nevbar edit and itinerary page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,32 +7,34 @@
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        <b-nav-item href="/main">Home</b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search Locations"></b-form-input>
           <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
         </b-nav-form>
 
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
+        <b-nav-item-dropdown text="Where to?">
+          <b-dropdown-item href="/itinerary">Itinerary</b-dropdown-item>
+          <b-dropdown-item href="#">Add Activity</b-dropdown-item>
+          <b-dropdown-item href="#">Map</b-dropdown-item>
         </b-nav-item-dropdown>
 
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
+        <!-- <b-nav-item-dropdown right> 
           <template #button-content>
-            <em>User</em>
+            <em><div class = "circle"></div><div class="profileCircle"></div></em>
           </template>
           <b-dropdown-item href="#">Profile</b-dropdown-item>
           <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
+        </b-nav-item-dropdown> -->
+
+          <b-navbar-nav>
+            <b-nav-item class = "circle"></b-nav-item>
+            <b-nav-item href="#" class = "profileCircle"></b-nav-item>
+          </b-navbar-nav>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -45,7 +47,34 @@ export default {
 </script>
 
 <style scoped>
- .navbar.navbar-dark.bg-dark{
+.navbar.navbar-dark.bg-dark{
     background-color: #b6ad90!important;
- }
+}
+
+.profileCircle{
+        position: relative;
+        top: 100%;
+        left: 100%;
+        margin-top:-40px;
+        margin-left:-60px;
+        width: 40px;
+        height: 40px;
+        -webkit-border-radius: 20px;
+        -moz-border-radius: 20px;
+        border-radius: 20px;
+        background: black;
+}
+.circle {
+        position: relative;
+        top: 100%;
+        left: 100%;
+        margin-top: -40px;
+        margin-left: -10px;
+        width: 40px;
+        height: 40px;
+        -webkit-border-radius: 20px;
+        -moz-border-radius: 20px;
+        border-radius: 20px;
+        background: red;
+}
 </style>
---
+++ pages/itinerary.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+    <Navbar />
+    <h1 style="color: #BC4749; font-family: merriweather; font-size: 50px; 
+    padding-top: 20px; padding-left: 175px; ">
+        <strong>
+        <i>
+            Itinerary
+        </i>
+        </strong>
+    </h1>
+
+    <b-container>
+        <div class="row-fluid scrollable" style="width:1000px; height:500px">
+            <div class="locations" v-for="imgUrl in images" :key="imgUrl">
+                <img class="square" :src="imgUrl">
+                <i class="fa-solid fa-ellipsis-vertical fa-2x"></i>
+
+            </div>
+        </div>
+    </b-container>
+
+
+    </div>
+    
+
+</template>
+
+<script>
+    import Navbar from "@/components/Navbar.vue";
+
+    export default {
+    name: 'Itinerary',
+    data() {
+        return {
+        images: [
+            "https://images.unsplash.com/photo-1623156346149-d5cec8b29818?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80", 
+            "https://images.unsplash.com/photo-1635563121451-2f3cfbf26826?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80",
+            "https://static.mothership.sg/1/2021/07/mos-burger-closed.jpeg",
+            "https://cdn.britannica.com/08/193908-050-66767D57/view-Subway-restaurant-Bangkok-Thailand.jpg",
+            "https://images.unsplash.com/photo-1532876688342-a79b7fa5c473?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1113&q=80",
+            "https://images.unsplash.com/photo-1635563121451-2f3cfbf26826?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80",
+            "https://images.unsplash.com/photo-1635563121451-2f3cfbf26826?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80",
+            "https://images.unsplash.com/photo-1621114957135-7f88c8447439?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80",
+            "https://images.unsplash.com/photo-1532876688342-a79b7fa5c473?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1113&q=80",
+            "https://images.unsplash.com/photo-1635563121451-2f3cfbf26826?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80",
+            "https://images.unsplash.com/photo-1635563121451-2f3cfbf26826?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80"
+        ],
+        texts: [
+            "McDonald's",
+            "Burger King",
+            "Mos Burger",
+            "Subway"
+        ]
+        }
+    },
+    components: { Navbar },
+}
+
+
+    
+</script>
+
+<style>
+    body{
+        background-color: #FCF6E7;
+    }
+
+    .locations {
+        background-color: maroon;
+        color: white;
+        border: none;
+        margin: 15px;
+        padding: 20px 50px;
+        cursor: pointer;
+        font-style: italic;
+        border-radius: 15px;
+    }
+
+    .scrollable {
+        overflow-y: auto;
+        white-space: nowrap;
+    }
+
+    .square {
+        width: 100px;
+        height: 100px;
+    }
+</style>
