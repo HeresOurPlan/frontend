59d74df65257602bb5ba7ba59fad3221d496cd9a
h41dr3l <84501740+h41dr3l@users.noreply.github.com>
2022-06-20 11:57:32 +0800

    multiple location marker with popup

--- pages/map.vue
+++ pages/map.vue
@@ -7,7 +7,8 @@
     <l-map style="height: 300px" :zoom="zoom" :center="center">
       <l-tile-layer :url="url" :attribution="attribution">
       </l-tile-layer>
-      <l-marker :lat-lng="markerLatLng">
+      <l-marker v-for="item in markers" :key="item" :lat-lng="item.coord">
+      <l-tooltip :content="item.name" :options="options"></l-tooltip>
       </l-marker>
     </l-map>
   </div>
@@ -17,17 +18,45 @@
 import Navbar from "@/components/Navbar.vue";
 
 export default {
+  
   data () {
     return {
+      location: 'Botanic Gardens',
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution:
         '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      zoom: 12,
-      center: [1.3521, 103.8198],
+      zoom: 11,
+      center: [1.3521,103.8198],
       // Singapore LatLng
-      markerLatLng: [1.3521, 103.8198]
+      markers: [{name: "Botanic Gardens", coord: [1.3138, 103.8159]},
+                {name:"Marina Bay Sands", coord: [1.2847, 103.8610]},
+                {name:"Universal Studios Singapore", coord: [1.2540, 103.8238]}]
     };
   },
-  components: { Navbar }
+  components: { Navbar },
+  // methods: { 
+  //   getlatlong: function() {
+  //       var dict = {
+  //       "Botanic Gardens": [1.3138, 103.8159],
+  //       "Marina Bay Sands" : [1.2847, 103.8610],
+  //       "Universal Studios Singapore" : [1.2540, 103.8238]
+  //       };
+  //       this.markerLatLng.push = dict[this.location];
+  //       this.center = dict[this.location];
+  //       console.log(this.location)
+  //       console.log(dict[this.location]);
+        
+  //     // console.log(this.location);
+  //     // console.log(dict[this.location]);
+  //     // return {
+  //     //   url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  //     //   attribution:
+  //     //     '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+  //     //   zoom: 12,
+  //     //   center: dict[this.location],
+  //     //   // Singapore LatLng
+  //     //   markerLatLng: dict[this.location]
+  //   }
+  //  }
 }
 </script>
